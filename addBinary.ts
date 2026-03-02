79959f2e8ffa4b790c7758fb822382e0ac17275c
Ianne Carl <bulilaniannecarl@gmail.com>
2026-03-02 23:43:50 +0800

    add binary 3/2/26

---
+++ addBinary.ts
@@ -0,0 +1,10 @@
+function addBinary(a: string, b: string): string {
+    
+    let convertedNum1:number = parseInt(a,2);
+    let convertedNum2:number = parseInt(b,2);
+
+    return (convertedNum1 + convertedNum2).toString(2);
+
+};
+
+console.log(addBinary("11", "1"))
