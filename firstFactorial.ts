92511ff3c7823db3c9730a678b862ad3e5751da8
Ianne Carl <bulilaniannecarl@gmail.com>
2026-03-06 23:00:18 +0800

    factorial 3/6/26

---
+++ firstFactorial.ts
@@ -0,0 +1,18 @@
+function FirstFactorial(num:number) { 
+let sum = 0;
+let firstLoop = true;
+  for(let i = num; i > 1; i--){
+    if(firstLoop){
+      sum  = i * (i-1);
+      firstLoop = false;
+      continue
+    }
+    sum = sum * (i - 1);
+  }
+
+return sum;
+
+}
+   
+// keep this function call here 
+console.log(FirstFactorial(5));
